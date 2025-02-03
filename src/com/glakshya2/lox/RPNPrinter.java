5add09366ac581f47c3bca2fc3b29bf8109c4a5d
glakshya2 <glakshya2@gmail.com>
2025-02-03 20:25:41 +0530

    Complete inheritance

--- src/com/glakshya2/lox/RPNPrinter.java
+++ src/com/glakshya2/lox/RPNPrinter.java
@@ -48,6 +48,16 @@ class RPNPrinter implements Expr.Visitor<String> {
     }
 
     @Override
+    public String visitSuperExpr(Expr.Super expr) {
+        return null;
+    }
+
+    @Override
+    public String visitThisExpr(Expr.This expr) {
+        return null;
+    }
+
+    @Override
     public String visitUnaryExpr(Expr.Unary expr) {
         return arrange(expr.operator.lexeme, expr.right);
     }
